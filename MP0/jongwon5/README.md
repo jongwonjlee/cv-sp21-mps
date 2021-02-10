be731c91cb61b2df1899c25140011982d487deba
jongwonjlee <jongwon5@illinois.edu>
2021-02-09 19:15:17 -0600

    add readme

--- MP0/jongwon5/README.md
+++ MP0/jongwon5/README.md
@@ -1,38 +1,183 @@
-design
+## MP0-Q3 by Jongwon Lee (jongwon5)
 
-class ImgUnshredder(src, dst):
+## Description on parameters
+```
+usage: main.py [-h] [--src-dir SRC_DIR] [--dst-dir DST_DIR]
+               [--img-name {simple_almastatue,simple_larry-roberts,hard_almastatue,hard_texture,hard_text,hard_building}]
+               [--mode {None,ssd,zncc}]
 
-"""
-member array_dict: dictionary of strips (keys: filename, values: array of image)
-member sort_order: deque of sort order from left to right
-member pixel_offset: deque of pixel offset from left to right
-"""
+parameters for image processing
 
-def combine_strips(self):
-"""
-param array_dict: dictionary of strips (keys: filename, values: array of image)
-param sort_order: deque of sort order from left to right
-return combined_img: combined strips
-"""
+optional arguments:
+  -h, --help            show this help message and exit
+  --src-dir SRC_DIR     directory to read shredded images
+  --dst-dir DST_DIR     directory to write concatenated image
+  --img-name {simple_almastatue,simple_larry-roberts,hard_almastatue,hard_texture,hard_text,hard_building}
+                        image to concatenate
+  --mode {None,ssd,zncc}
+                        matching method to use
+```
 
-def sort_strips(self, mode="ssd"):
-"""
-param array_dict
-param mode: mode of comparison (either ssd or zncc)
-return sort_order
-"""
+## How to use
 
-def _ssd(self, img1, img2):
-"""
-param img1: first array, whose right side will be investigated
-param img2: second array, whose left side will be investigated
-return ssd: sum of squared L2 distance for all pairs of pixels
-"""
+Before get started, please unzip this project into the directory `MP0/`, which should already include a folder `shredded-images/`.
 
-def _zncc(self, img1, img2):
-"""
-param img1: first array, whose right side will be investigated
-param img2: second array, whose left side will be investigated
-return ssd: AVERAGE zero mean normalized cross correlation for all pairs of pixels (CAUTION: only the average values for non-zero pixels should be applied)
-"""
+1. To create the answer for (3.1.), please execute:
 
+```
+python main.py --img-name simple_{IMGNAME}
+```
+
+2. To create the answer for (3.2.), please execute:
+
+```
+python main.py --img-name simple_{IMGNAME} --mode ssd
+```
+
+3. To create the answer for (3.3.), please execute:
+
+```
+python main.py --img-name hard_{IMGNAME} --mode zncc
+```
+
+You may be able to see combined strips in the directory `results/`.
+
+## Examples
+
+Let's reconstruct the Alma Statue.
+
+1. Combine [5 pts]
+
+```
+$ python main.py --img-name simple_almastatue 
+> Namespace(dst_dir='results/', img_name='simple_almastatue', mode=None, src_dir='../shredded-images/')
+> collapsed time: 0.0000 [s]
+```
+
+![simple_almastatue_random](results/simple_almastatue_random.png)
+
+You may be able to see the weird combined image in an unsorted order. This has been done by simply reading all strips in the designated directory (function `_read_strips(self, src_dir)`), sort randomly (function `sort_strips_random(self)`), and combine these (function `combine_strips(self)`). 
+
+
+2. Re-order [10 pts]
+
+Now, let's sort the strips using *the sum of squared differences*.
+
+```
+$ python main.py --img-name simple_almastatue --mode ssd
+> Namespace(dst_dir='results/', img_name='simple_almastatue', mode='ssd', src_dir='../shredded-images/')
+> collapsed time: 0.0247 [s]
+```
+
+![simple_almastatue](results/simple_almastatue.png)
+
+It seems like the strips are sorted properly. The sorting algorithm is implemented in a member function `sort_strips_ssd` of `ImgUnshredder` class. Below is the pseudocode of `sort_strips_ssd` function.
+
+```
+input : strips_unsorted, strips_sorted
+goal  : move all elements from strips_unsorted to strips_sorted with a proper order.
+
+do 
+    for all strip_being_compared in strips_unsorted:
+        max_nssd <- -inf
+        similarest_strip <- None
+        append_direction <- None
+        
+        if nssd(rightmost(strips_sorted), leftmost(strip_being_compared)) > max_nssd then
+            max_nssd <- nssd(rightmost(strips_sorted), leftmost(strip_being_compared))
+            similarest_strip <- strip_being_compared
+            append_direction <- right
+        else if nssd(rightmost(strip_being_compared), leftmost(strips_sorted)) > max_nssd then
+            max_nssd <- nssd(rightmost(strip_being_compared), leftmost(strips_sorted))
+            similarest_strip <- strip_being_compared
+            append_direction <- left
+        end
+    end
+
+    move similarest_strip from strips_unsorted to strips_sorted, by appending on the append_direction
+
+until len(strips_unsorted) == 0
+```
+
+where negative SSD can be computed as below:
+
+```
+function nssd(arr1, arr2)
+    return -sum(l2_norm(arr1 - arr2))
+end
+```
+
+If we assume that there are n strips to be sorted, this greedy SSD algorithm as the time complexity of *O(n\*\*2)*, since it needs to compare (n-1) + (n-2) + ... + 1 strips for all iterations.
+
+Except Alma Statue, Larry Roberts's picture as been successfully reconstructed as well.
+
+![simple_larry-roberts](results/simple_larry-roberts.png)
+
+3. Align and Re-order [15 pts]
+
+Now let's move on to the unsorted strips with offset and try to reconstruct it based on *zero mean normalized cross correlation* algorithm.
+
+```
+$ python main.py --img-name hard_almastatue --mode zncc
+> Namespace(dst_dir='results/', img_name='hard_almastatue', mode='zncc', src_dir='../shredded-images/')
+> collapsed time: 160.3736 [s]
+```
+
+![hard_almastatue](results/hard_almastatue.png)
+
+It seems like the strips are properly aligned sorted as well. The sorting algorithm is implemented in a member function `sort_strips_zncc` of `ImgUnshredder` class. Below is the pseudocode of `sort_strips_zncc` function.
+
+```
+input : strips_unsorted, strips_sorted, relative_offset
+goal  : move all elements from strips_unsorted to strips_sorted with a proper order, while recording the relative offset between left to right strips in relative_offset.
+
+do 
+    for all strip_being_compared in strips_unsorted:
+        max_zncc <- 0
+        similarest_strip <- None
+        similarest_offset <- None
+        append_direction <- None
+        
+        for all offset in offset_range
+            if zncc(rightmost(strips_sorted), leftmost(strip_being_compared), offset) > max_zncc then
+                max_zncc <- -ssd(rightmost(strips_sorted), leftmost(strip_being_compared))
+                similarest_strip <- strip_being_compared
+                similarest_offset <- offset
+                append_direction <- right
+            else if -ssd(rightmost(strip_being_compared), leftmost(strips_sorted), offset) > max_zncc then
+                max_zncc <- -ssd(rightmost(strip_being_compared), leftmost(strips_sorted))
+                similarest_strip <- strip_being_compared
+                similarest_offset <- offset
+                append_direction <- left
+            end
+        end
+    end
+
+    move similarest_strip from strips_unsorted to strips_sorted, by appending on the append_direction
+    add similarest_offset in relative_offset, by appending on the append_direction
+
+until len(strips_unsorted) == 0
+```
+
+where ZNCC can be computed as below:
+
+```
+function zncc(arr1, arr2)
+    sum_zncc = 0
+    for n in range(size(arr1)):
+        sum_zncc += (arr1[n] - avg(arr1)) * (arr2[n] - avg(arr2)) / (std(arr1) * std(arr2))
+    
+    return sum_zncc / size(arr1)
+end
+```
+
+It takes significantly longer time than SSD algorithm. This is due to the fact that ZNCC with offset algorithm has the time complexity of *O(n\*\*2 * o)* whereas SSD has *O(n\*\*2)* (*n* : the number of strips, *o* : the pixel offset to be investigated), which may have resulted in such a significant computation burden.
+
+
+Like Alma Statue, Other unsorted and misaligned images has been successfully reconstructed as well.
+
+![hard_building](results/hard_building.png)
+
+![hard_text](results/hard_text.png)
+
+![hard_texture](results/hard_texture.png)
--- MP0/jongwon5/main.py
+++ MP0/jongwon5/main.py
@@ -1,79 +1,85 @@
-import sys, os
-import numpy as np
+import os, time
 import cv2
+import numpy as np
+
 import argparse
 from collections import deque
 
-import random
-import time
-
 class ImgUnshredder:
-    def __init__(self, src):
-        """
-        member array_dict: dictionary of strips (keys: filename, values: array of image)
-        member sort_order: deque of sort order from left to right
-        member relative_offset: deque of pixel offset from left to right
-        """
-        self.src = src
-        self.strips_unsorted = dict()
-        self.strips_sorted = deque()
-        self.relative_offset = deque()
+    def __init__(self, src_dir):
+        self.strips_unsorted = dict()   # dictionary of unsorted strips (keys: filename, values: array of strip data)
+        self.strips_sorted = deque()    # deque of sorted strips from left to right
+        self.relative_offset = deque()  # deque of relative pixel offset from left to right
         
-        self._read_strips()
+        # size of canvas to save combined strips
+        self.H = None
+        self.W = None
+        self.C = None
+
+        # read strips in `src_dir` and prepare to sorting
+        self._read_strips(src_dir)
+        self.strips_sorted.append(self.strips_unsorted.popitem()[-1])
 
     def combine_strips(self):
         """
-        param array_dict: dictionary of strips (keys: filename, values: array of image)
-        param sort_order: deque of sort order from left to right
-        return combined_img: combined strips
+        description : combine images in the order of self.strips_sorted and relative offset of self.relative_offset
+        return combined_img : combined strips
         """
+        # create a canvas to save the result
         combined_img = np.zeros([self.H, self.W, self.C])
         
-        w = 0
-        d = 0
+        # x: current horizontal position at which combined_img is painted
+        # y: current vertical position at which combined_img is painted (calculated by accumulating relative offset over different strips)
+        x = 0
+        y = 0
         for i, img in enumerate(self.strips_sorted):
             try:
                 offset = self.relative_offset[i]
             except:
                 offset = 0
-            H, W, _ = np.shape(img)
-            combined_img[max(d, 0) : min(H + d, self.H), w : w + W, :] \
-                = img[max(-d, 0): min(self.H - d, H), :, :]
-            w = w + W
-            d = d + offset
+            h, w, _ = np.shape(img)
+            combined_img[max(y,0):min(h+y,self.H), x:x+w, :] = img[max(-y,0): min(self.H-y,h), :, :]
+            x += w
+            y += offset
         return combined_img
         
     def sort_strips_random(self):
+        """
+        description : sort strips randomly
+        """
         while not len(self.strips_unsorted) == 0:
             self.strips_sorted.append(self.strips_unsorted.popitem()[1])
 
     def sort_strips_ssd(self):
         """
-        param array_dict
-        return sort_order
+        description : sort strips using (negative of) the sum of squared differences
         """
+        # do until all strips are sorted
         while not len(self.strips_unsorted) == 0:
             similarest_filename = None
             similarest_score = -np.inf
             similarest_direction = None
+
+            # compute SSD for every unsorted strip w.r.t. the set of sorted strips
             for filename, img in self.strips_unsorted.items():
-                # check right side first
+                # (1) check SSD between the rightmost side of sorted strips and a randomly selected strip from the unsorted set
                 arr1 = np.float32(self.strips_sorted[-1][:, -1, :])
                 arr2 = np.float32(img[:, 0, :])
-                ssd = self._ssd(arr1, arr2)
-                if similarest_score < ssd:
+                nssd = self._nssd(arr1, arr2)
+                if similarest_score < nssd:
                     similarest_filename = filename
-                    similarest_score = ssd
+                    similarest_score = nssd
                     similarest_direction = "right"
-                # check left side
+                # (2) check SSD between the randomly selected strip from the unsorted set and the leftmost side of sorted strips
                 arr1 = np.float32(img[:, -1, :])
                 arr2 = np.float32(self.strips_sorted[0][:, 0, :])
-                ssd = self._ssd(arr1, arr2)
-                if similarest_score < ssd:
+                nssd = self._nssd(arr1, arr2)
+                if similarest_score < nssd:
                     similarest_filename = filename
-                    similarest_score = ssd
+                    similarest_score = nssd
                     similarest_direction = "left"
             
+            # move the selected strip from the unsorted set to the sorted set
             if similarest_direction == "left":
                 self.strips_sorted.appendleft(self.strips_unsorted.pop(similarest_filename))
             else:
@@ -81,22 +87,26 @@ class ImgUnshredder:
 
     def sort_strips_zncc(self):
         """
-        param array_dict
-        return sort_order
+        description : sort strips using zero-mean normalized cross correlation
         """
+        # do until all strips are sorted
         while not len(self.strips_unsorted) == 0:
             similarest_filename = None
             similarest_score = -np.inf
             similarest_direction = None
             similarest_offset = None
+
+            # compute ZNCC for every unsorted strip w.r.t. the set of sorted strips
             for filename, img in self.strips_unsorted.items():
-                # check right side
+                # (1) ZNCC between the rightmost side of sorted strips and a randomly selected strip from the unsorted set
                 strip1 = np.expand_dims(self.strips_sorted[-1][:, -1, :], axis=1)
                 strip2 = np.expand_dims(img[:, 0, :], axis=1)
                 strip1 = cv2.cvtColor(strip1, cv2.COLOR_BGR2GRAY)
                 strip2 = cv2.cvtColor(strip2, cv2.COLOR_BGR2GRAY)
                 strip1 = strip1.astype(np.float32)
                 strip2 = strip2.astype(np.float32)
+
+                # (1-1) compare all ZNCC with offset ranging from -offset_max to offset_max
                 offset_max = int(strip1.size * 0.2)
                 for offset in range(-offset_max, offset_max):
                     arr1 = strip1[max(0, offset): min(strip1.size, strip2.size + offset)].squeeze()
@@ -107,13 +117,15 @@ class ImgUnshredder:
                         similarest_score = zncc
                         similarest_direction = "right"
                         similarest_offset = offset
-                # check left side
+                # (2) ZNCC between the randomly selected strip from the unsorted set and the leftmost side of sorted strips
                 strip1 = np.expand_dims(img[:, -1, :], axis=1)
                 strip2 = np.expand_dims(self.strips_sorted[0][:, 0, :], axis=1)
                 strip1 = cv2.cvtColor(strip1, cv2.COLOR_BGR2GRAY)
                 strip2 = cv2.cvtColor(strip2, cv2.COLOR_BGR2GRAY)
                 strip1 = strip1.astype(np.float32)
                 strip2 = strip2.astype(np.float32)
+
+                # (2-1) compare all ZNCC with offset ranging from -offset_max to offset_max
                 offset_max = int(strip1.size * 0.2)
                 for offset in range(-offset_max, offset_max):
                     arr1 = strip1[max(0, offset): min(strip1.size, strip2.size + offset)].squeeze()
@@ -125,6 +137,7 @@ class ImgUnshredder:
                         similarest_direction = "left"
                         similarest_offset = offset
             
+            # move the selected strip from the unsorted set to the sorted set and record the relative offset between the left to the right
             if similarest_direction == "left":
                 self.strips_sorted.appendleft(self.strips_unsorted.pop(similarest_filename))
                 self.relative_offset.appendleft(similarest_offset)
@@ -132,43 +145,40 @@ class ImgUnshredder:
                 self.strips_sorted.append(self.strips_unsorted.pop(similarest_filename))
                 self.relative_offset.append(similarest_offset)
     
-    def _read_strips(self):
+    def _read_strips(self, src_dir):
         """
-        param src: source directory to read strips
-        return array_dict: dictionary of strips (keys: filename, values: array of image)
+        description : read strips in `src_dir` and save them in self.strips_unsorted, a dictionary with keys of filename and values of image data
+        param src_dir : source directory from which strips are read
         """
-        try:
-            os.path.exists(self.src)
-        except IOError:
-            print("FILE DOES NOT EXIST IN ", self.src)
+        assert os.path.exists(src_dir)
         
+        # read each strip to decide the canvas size and add filename and strip data to self.strips_unsorted
         self.W = 0
         self.H = 0
-        for filename in os.listdir(self.src):
-            img = cv2.imread(os.path.join(self.src, filename))
+        for filename in os.listdir(src_dir):
+            img = cv2.imread(os.path.join(src_dir, filename))
             self.strips_unsorted[filename] = img
             h, w, self.C = np.shape(img)
             self.W = self.W + w
             self.H = max(self.H, h)
         
-        self.strips_sorted.append(self.strips_unsorted.popitem()[-1])
-            
-
-    def _ssd(self, arr1, arr2):
+    def _nssd(self, arr1, arr2):
         """
-        param arr1: first array to be investigated
-        param arr2: second array to be investigated
-        return ssd: NEGATIVE sum of squared L2 distance for all pairs of pixels
+        param arr1 : first array to be investigated (shape: (?, 3))
+        param arr2 : second array to be investigated (shape: (?, 3))
+        return nssd : NEGATIVE sum of squared L2 distance for all pairs of pixels
         """
+        assert arr1.shape == arr2.shape
+        assert np.shape(arr1)[-1] == np.shape(arr2)[-1] == 3
         assert arr1.dtype == np.dtype(np.float32) and arr2.dtype == np.dtype(np.float32)
         
         return -np.sum(np.linalg.norm(arr1 - arr2, axis=1))
 
     def _zncc(self, arr1, arr2):
         """
-        param arr1: first array to be investigated
-        param arr2: second array to be investigated
-        return zncc: AVERAGE zero mean normalized cross correlation for all pairs of pixels (CAUTION: only the average values for non-zero pixels should be applied)
+        param arr1 : first array to be investigated (shape: (?,))
+        param arr2 : second array to be investigated (shape: (?,))
+        return avg_zncc : AVERAGE zero mean normalized cross correlation for all pairs of pixels
         """
         assert arr1.shape == arr2.shape
         assert arr1.ndim == 1 and arr2.ndim == 1
@@ -188,19 +198,21 @@ class ImgUnshredder:
         
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='parameters for image processing')
-    parser.add_argument('--src-path', type=str, help='directory to read shredded images', default="MP0/shredded-images/")
-    parser.add_argument('--dst-path', type=str, help='directory to write concatenated image', default="MP0/jongwon5/result")
+    parser.add_argument('--src-dir', type=str, help='directory to read shredded images', default="../shredded-images/")
+    parser.add_argument('--dst-dir', type=str, help='directory to write concatenated image', default="results/")
     parser.add_argument('--img-name', type=str, help='image to concatenate', \
         choices=['simple_almastatue', 'simple_larry-roberts', 'hard_almastatue', 'hard_texture', 'hard_text', 'hard_building'])
-    parser.add_argument('--mode', type=str, help='mode to use', choices=[None, 'ssd', 'zncc'], default=None)
+    parser.add_argument('--mode', type=str, help='matching method to use', choices=[None, 'ssd', 'zncc'], default=None)
 
     args = parser.parse_args()
+    print(args)
 
     # create a ImgUnshredder object
-    src = os.path.join(args.src_path, args.img_name)
-    unshredder = ImgUnshredder(src)
+    src_dir = os.path.join(args.src_dir, args.img_name)
+    unshredder = ImgUnshredder(src_dir)
 
     # do corresponding sorting method: None, ssd, or zncc
+    tic = time.time()
     if args.mode == None:
         unshredder.sort_strips_random()
         args.img_name += "_random"
@@ -208,12 +220,13 @@ if __name__ == "__main__":
         unshredder.sort_strips_ssd()
     elif args.mode == "zncc":
         unshredder.sort_strips_zncc()
-    
+    print(f"collapsed time: {time.time() - tic:.4f} [s]")
+
     # combine sorted strips
     img = unshredder.combine_strips()
 
     # save the result
-    if not os.path.exists(args.dst_path):
-        os.makedirs(args.dst_path)
-    cv2.imwrite(os.path.join(args.dst_path, args.img_name + ".png"), img)
+    if not os.path.exists(args.dst_dir):
+        os.makedirs(args.dst_dir)
+    cv2.imwrite(os.path.join(args.dst_dir, args.img_name + ".png"), img)
     
