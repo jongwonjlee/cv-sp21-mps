a179af6060cd41446962c5168c13911afc29803b
jongwonjlee <jongwon5@illinois.edu>
2021-02-13 18:02:21 -0600

    completed q4

---
+++ MP1/jongwon5/README.md
@@ -0,0 +1,10 @@
+## Question 3
+
+
+## Question 4
+
+1. road, tz > 0
+2. road, tx > 0
+3. wall, tz > 0
+4. wall, tz, tx, ty > 0
+5. wall, wy > 0
--- MP1/jongwon5/q4_code/dynamic_perspective_starter.py
+++ MP1/jongwon5/q4_code/dynamic_perspective_starter.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -17,10 +18,14 @@ def get_road_z_image(H_val, fx, fy, cx, cy, szx, szy):
 
 
 def plot_optical_flow(ax, Z, u, v, cx, cy, szx, szy, s=16):
-    # Here is a function for plotting the optical flow. Feel free to modify this 
-    # function to work well with your inputs, for example if your predictions are
-    # in a different coordinate frame, etc.
-
+    """
+    param ax : object for plotting a figure
+    param Z : scene to be observed
+    param u : magnitude of optical flow along x axis
+    param v : magnitude of optical flow along y axis
+     
+    """
+    # Visualize optical flow map
     x, y = np.meshgrid(np.arange(szx), np.arange(szy))
     ax.imshow(Z, alpha=0.5, origin='upper')
     q = ax.quiver(x[::s,::s], y[::s,::s], u[::s,::s], -v[::s, ::s])
@@ -32,8 +37,43 @@ def plot_optical_flow(ax, Z, u, v, cx, cy, szx, szy, s=16):
     ax.set_ylim([szy, 0])
     ax.axis('equal')
 
+def create_optical_flow(Z, fx, fy, cx, cy, szx, szy, t, w, eps=.001):
+    """
+    param Z: depth map
+    param t: translational velocity (list or np array with (3,))
+    param w: rotational velocity (list or np array with (3,))
+    """
+    # Cast t and w to be column vectors
+    if type(t) == list or t.shape != (3, 1):
+        t = np.array(t)[:, np.newaxis]
+    if type(w) == list or w.shape != (3, 1):
+        w = np.array(w)[:, np.newaxis]
+    
+    u = np.random.rand(*Z.shape)
+    v = np.random.rand(*Z.shape)
+
+    # Fill data
+    for y in range(szy):
+        for x in range(szx):
+            depth = Z[y, x]           # depth to the object from camera center
+            X = depth / fx * (x - cx)  # X coordinate of the object w.r.t. the camera center
+            Y = depth / fy * (y - cy)  # Y coordinate of the object w.r.t. the camera center
+            
+            # Compute optical flow
+            T = np.array([[-1, 0, X], 
+                        [0, -1, Y]])
+            R = np.array([[X*Y, -(1+X**2),  Y],
+                        [(1+Y**2), -X*Y, -X]])
+            of = T @ t / (depth + eps) + R @ w
+            u[y, x] = of[0]
+            v[y, x] = of[1]
+        
+    return u, v
 
 if __name__ == "__main__":
+    # Current code directory
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+
     # Focal length along X and Y axis. In class we assumed the same focal length 
     # for X and Y axis. but in general they could be different. We are denoting 
     # these by fx and fy, and assume that they are the same for the purpose of
@@ -49,20 +89,34 @@ if __name__ == "__main__":
     cx = 192
     cy = 128
 
-    # Gets the image of a wall 2m in front of the camera.
-    Z1 = get_wall_z_image(2., fx, fy, cx, cy, szx, szy)
-
-
     # Gets the image of the ground plane that is 3m below the camera.
-    Z2 = get_road_z_image(3., fx, fy, cx, cy, szx, szy)
+    Z1 = get_road_z_image(3., fx, fy, cx, cy, szx, szy)
+    # Gets the image of a wall 2m in front of the camera.
+    Z2 = get_wall_z_image(2., fx, fy, cx, cy, szx, szy)
 
+    # Plot Z1 and Z2
     fig, (ax1, ax2) = plt.subplots(1,2, figsize=(14,7))
     ax1.imshow(Z1)
     ax2.imshow(Z2)
+    plt.savefig(os.path.join(current_dir, 'map.png'))
 
-    # Plotting function.
-    f = plt.figure(figsize=(13.5,9))
-    u = np.random.rand(*Z1.shape)
-    v = np.random.rand(*Z1.shape)
-    plot_optical_flow(f.gca(), Z1, u, v, cx, cy, szx, szy, s=16)
-    f.savefig('optical_flow_output.pdf', bbox_inches='tight')
+    # Create data to simulate
+    dataset = {
+        '01': {'Z': Z1, 't': [0,0,1], 'w':[0,0,0]},
+        '02': {'Z': Z1, 't': [1,0,0], 'w':[0,0,0]},
+        '03': {'Z': Z2, 't': [0,0,1], 'w':[0,0,0]},
+        '04': {'Z': Z2, 't': [1,1,1], 'w':[0,0,0]},
+        '05': {'Z': Z2, 't': [0,0,0], 'w':[0,1,0]},
+        }
+    
+    for data_name, data in dataset.items():
+        Z = data['Z']
+        t = data['t']
+        w = data['w']
+        
+        u, v = create_optical_flow(Z, fx, fy, cx, cy, szx, szy, t, w)
+        
+        # Plot the obtained optical flow
+        f = plt.figure(figsize=(13.5,9))
+        plot_optical_flow(f.gca(), Z, u, v, cx, cy, szx, szy, s=16)
+        f.savefig(os.path.join(current_dir, f'{data_name}.pdf'), bbox_inches='tight')
